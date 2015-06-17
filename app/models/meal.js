8ca6b1f04b4d0cae714459489e54c00558ea3a6f
Ernesto Soberanes <netosober@gmail.com>
2015-06-17 06:25:47 -0400

    Setup application

---
+++ app/models/meal.js
@@ -0,0 +1,8 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  description: DS.attr('string'),
+  calories: DS.attr('number'),
+  day: DS.attr('date'),
+  hour: DS.attr('date')
+});
--- app/router.js
+++ app/router.js
@@ -6,6 +6,12 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.resource('meals', function() {
+    this.resource('meal', { path: '/:meal_id' });
+  });
+  this.route('mealss', function() {});
+  this.route('meals', function() {});
+  this.route('meal');
 });
 
 export default Router;
